65a5de317ab9daebb7a0ea73a651fe2a838bf2e0
shanmuk49 <84376799+shanmuk49@users.noreply.github.com>
2021-06-17 19:28:51 +0530

    Add files via upload

---
+++ pattern11.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class patter10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(char i = 'E'; i>='A';i--) 
+		{
+		for (int j=1; j <=5; j++) 
+		{
+			System.out.print(i);			
+		}
+		System.out.println();
+		}
+	}}
